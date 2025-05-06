2a7d98d6057ba14e2501114a6a5574c5781d2b53
Bhargava Ram A <122313187+BhargavaRam123@users.noreply.github.com>
2025-05-06 15:02:05 +0530

    add JSON parsing and type checks in basics.ts; update variable assignments

--- day1/basics.js
+++ day1/basics.js
@@ -2,3 +2,5 @@ var a = 1; //implicit type declaration
 // a = "string"
 var b = 12;
 b = "hello";
+var json = JSON.parse("{}");
+console.log(typeof json);
--- day1/basics.ts
+++ day1/basics.ts
@@ -4,3 +4,19 @@ let a = 1 //implicit type declaration
 
 let b:any = 12
 b = "hello"
+
+const json = JSON.parse("{}")
+console.log(typeof json);
+
+
+function func(val:any)
+{
+    console.log(val.toUpperCase())
+}
+
+func("hello")
+function func1(val:unknown)
+{
+    if(typeof val === "string")
+    console.log(val.toUpperCase())
+}
