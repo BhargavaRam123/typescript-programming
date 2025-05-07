36b53de54f16e4d96a64b6983d768a3281fabdac
Bhargava Ram A <122313187+BhargavaRam123@users.noreply.github.com>
2025-05-07 11:24:24 +0530

    add Chef class extending Person with overridden greet method

--- inheritance.ts
+++ inheritance.ts
@@ -5,8 +5,19 @@ class Person{
         this.name = name 
         this.age= age  
     }
-    greet()
+    greet():void
     {
         console.log(`Hi myself ${this.name}, i am ${this.age} years old`);
+    }
+}
+class Chef extends Person{
+    occupation:string 
+    constructor(name:string,age:number,occupation:string )
+    {
+        super(name,age)
+        this.occupation =occupation 
+    }
+    greet(): void {
+        console.log(`Hi i am a ${this.occupation}`);
     }
 }
