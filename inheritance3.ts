98a23db068dd1e515cc4246607bccf27acf68e6d
Bhargava Ram A <122313187+BhargavaRam123@users.noreply.github.com>
2025-05-07 12:55:33 +0530

    deadly diamond of death in ts

--- inheritance3.ts
+++ inheritance3.ts
@@ -1,9 +1,14 @@
-class p2{
+// class p2{
 
-}
-class p3{
+// }
+// class p3{
 
-}
-class p5 extends p2,p3{
-    
-}
+// }
+// class p5 extends p2,p3{
+
+// }
+
+// class A {};
+// class B extends A {};
+// class C extends A {};
+// class D extends B, C {};// This will throw an error
