21b92f9096f33e78643e0b434291d1f2bf14e71a
Bhargava Ram A <122313187+BhargavaRam123@users.noreply.github.com>
2025-05-07 10:40:10 +0530

    add task management functionality with TypeScript

---
+++ problem2.ts
@@ -0,0 +1,50 @@
+
+enum taskstatus{
+    completed = "true",
+    notcompleted = "false"
+   }
+   
+   interface Task{
+       id:number ,
+       title:string,
+       description:string,
+       completionstatus:taskstatus
+   }
+   
+   class taskmanager{
+       private tasks:Task[]
+       private counter:number
+       constructor(){
+           this.counter = 0 
+           this.tasks = []
+       }
+       addtask(title:string,description:string)
+       { 
+           let obj:Task ={
+               id:this.counter,
+               title,
+               description,
+               completionstatus:taskstatus.notcompleted
+           }
+           this.counter++
+           this.tasks.push(obj)
+       }
+       marktaskasdone(id:number)
+       {
+           this.tasks = this.tasks.map((obj)=>
+            obj.id === id ? {...obj,completionstatus:taskstatus.completed}:obj
+           )
+       }
+       getalltasks()
+       {
+        this.tasks.map((obj)=>{
+            console.log("Task->",obj);
+        })
+       }
+   }
+
+
+   const taskm = new taskmanager()
+   taskm.addtask("drink water","please drink water at 10:00am")
+   taskm.addtask("eat food","please take food at 13:00")
+   taskm.getalltasks()
