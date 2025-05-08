bc08db99a041bf94302f1f8d0994915beecc2944
Bhargava Ram A <122313187+BhargavaRam123@users.noreply.github.com>
2025-05-08 12:02:37 +0530

    refactor: update logger implementation and package dependencies

--- winston/src/index.ts
+++ winston/src/index.ts
@@ -1,1 +1,14 @@
-console.log("hello world");
+import { createLogger,transports,format } from "winston";
+
+const logger = createLogger({
+    transports:[new transports.Console()],
+    format:format.combine(
+        format.colorize(),
+        format.timestamp(),
+        format.printf(({timestamp,level,message})=>{
+            return `[${timestamp}] ${level}: ${message}`
+        })
+    )
+})
+
+logger.info('hello world!')
