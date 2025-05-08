29b8acd907f2079ccc9471e904692f10f28a3ae9
Bhargava Ram A <122313187+BhargavaRam123@users.noreply.github.com>
2025-05-08 12:47:12 +0530

    refactor: update logger configuration to use file transport and improve message formatting

--- winston/src/index.ts
+++ winston/src/index.ts
@@ -1,18 +1,37 @@
 import { createLogger,transports,format } from "winston";
 
+// const logger = createLogger({
+//     transports:[new transports.Console()],
+//     format:format.combine(
+//         format.colorize(),
+//         format.timestamp(),
+//         format.printf(({timestamp,level,message,service})=>{
+//             return `[${timestamp}] ${service} ${level}: ${message}`
+//         })
+//     ),
+//     defaultMeta:{
+//         service:"WinstonExample"
+//     }
+// }
+// )
+
+// logger.info('hello world!')
+
 const logger = createLogger({
-    transports:[new transports.Console()],
+    transports:[
+        new transports.File({
+            dirname:"logs",
+            filename:"winston_example.log"
+        })
+    ],
     format:format.combine(
-        format.colorize(),
         format.timestamp(),
         format.printf(({timestamp,level,message,service})=>{
-            return `[${timestamp}] ${service} ${level}: ${message}`
+            return `[${timestamp} ${service} ${level}: ${message}]`
         })
     ),
     defaultMeta:{
-        service:"WinstonExample"
+        service:"winstonexample",
     }
-}
-)
-
-logger.info('hello world!')
+})
+logger.info("helloworld")
